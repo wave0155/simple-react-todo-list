5dd55ab63c3ccf5465e06aa52b18cfd97e0cbbb4
wave0155 <wave0155@hotmail.com>
2019-06-12 09:59:00 +0700

    add task doesn't work

--- app/components/TaskAdder.js
+++ app/components/TaskAdder.js
@@ -1,17 +1,25 @@
-import { React } from 'react';
+import React from 'react';
 import { View, Text } from 'react-native';
+import { Overlay } from 'react-native-elements';
 
 export default class TaskAdder extends React.Component {
   constructor(props) {
     super(props);
     this.state={
-      isVisible: props.isVisible,
+      isAdderVisible: props.isAdderVisible,
     }
   }
+
   render() {
     return (
-      <Overlay isVisible={this.state.isVisible}>
-        
+      <Overlay 
+       isVisible={this.state.isAdderVisible}
+       onBackdropPress={() => {
+        this.setState({ isAdderVisible: false });
+        // this.props.parentStateHandler(false);
+       }}
+       >
+         <Text>Overlay Test</Text>
       </Overlay>
     );
   }
--- app/screens/CheckScreen.js
+++ app/screens/CheckScreen.js
@@ -1,6 +1,7 @@
 import React from 'react';
-import { View, Text } from 'react-native';
-import { ListItem } from 'react-native-elements';
+import { View, Text, StyleSheet } from 'react-native';
+import { ListItem, Overlay, Input, Icon, Button } from 'react-native-elements';
+import { Fab } from 'native-base';
 
 export default class CheckScreen extends React.Component {
   constructor(props) {
@@ -11,6 +12,8 @@ export default class CheckScreen extends React.Component {
         {name: 'Walk the Dog'},
         {name: 'Meet the Boss'}
       ],
+      isAdderVisible: false,
+      adderInput: null,
     }
   }
 
@@ -18,9 +21,18 @@ export default class CheckScreen extends React.Component {
     this.state.task.push(taskName)
   }
 
+  // renderTask = () => {
+  //   this.state.task.map((item, index) => (
+  //     <ListItem
+  //      key={index}
+  //      title={item.name}/>
+  //   ))
+  // }
+
   render() {
     return (
-      <View>
+      <View style={styles.container}>
+        <View>
         {
           this.state.task.map((item, index) => (
             <ListItem
@@ -28,7 +40,63 @@ export default class CheckScreen extends React.Component {
              title={item.name}/>
           ))
         }
+        </View>
+        <Fab 
+          position='bottomRight'
+          active='true'
+          style={{ backgroundColor: '#5067FF' }}
+          onPress={() => this.setState({ isAdderVisible: true })}
+        >
+          <Icon name='plus' type='font-awesome' color='white'/>
+        </Fab>
+
+        <Overlay
+         height={400}
+         isVisible={this.state.isAdderVisible}
+         onBackdropPress={() => this.setState({ isAdderVisible: false })}
+        >
+          <View style={styles.overlayView}>
+            <View>
+              <Text style={{fontSize: 30, textAlign: 'center', fontWeight: 'bold'}}>Add Task</Text>
+            </View>
+            <View style={styles.addTaskInput}>
+              <Input
+                placeholder='Task Name'
+                leftIcon={{ type: 'font-awesome', name: 'tasks' }}
+                onChangeText={(text) => this.setState({adderInput: text})}
+              />
+            </View>
+            <View style={{flexDirection: 'row', justifyContent: 'space-evenly'}}>
+              <Button
+                title='Add Task'
+                onPress={() => {
+                  this.setState({ task: [...this.state.task, this.state.adderInput]} )
+                  this.setState({ isAdderVisible: false })
+                  this.setState({ adderInput: null })
+                }}
+              />
+              <Button
+                title='Cancel'
+                onPress={() => this.setState({ isAdderVisible: false })}
+              />
+            </View>
+          </View>
+        </Overlay>
       </View>
     );
   }
 }
+
+const styles=StyleSheet.create({
+  container: {
+    flex: 1,
+  },
+  overlayView: {
+    flex: 1,
+    justifyContent: 'space-evenly',
+    // marginVertical: 50,
+  },
+  addTaskInput: {
+    
+  }
+});
